806df4f74fa5161eb8dbb8586ec1eb635228be36
nicktripp <nick.e.tripp@gmail.com>
2018-09-19 16:13:38 -0600

    Hacking to get a Mapbox to display

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+// Import App components
+
+const App = () => (
+    <div>
+        {/*
+         * App components
+         */}
+    </div>
+)
+
+export default App;
+
+
+/*
+App state:
+{
+    map: {
+        zoom: int,
+        coords: {
+            lat: float,
+            long: float,
+        },
+        points: [
+            {nodes?}
+        ]
+    },
+
+    search: {
+        input: str
+    },
+
+    resultList: {
+        isVisible: bool,
+        results: [
+            result1: {
+                name: str
+                coords: {...}
+            },
+            result2: {...},
+        ]
+    }
+
+
+}
+
+
+*/
